177299ffcef2b07b629a80a14247343389d2483e
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-04-11 13:12:35 -0300

    faet: add login page

--- webapp/src/pages/login/index.tsx
+++ webapp/src/pages/login/index.tsx
@@ -1,30 +1,59 @@
 import { useNavigate } from "react-router-dom";
 import Api from "../../api/api";
 import { saveToken } from "../../services/auth.service";
+import { useState } from "react";
+import styles from "./login.module.css";
 
 const LoginPage = () => {
   const api = new Api();
 
   const navigate = useNavigate();
 
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  });
+
   const handleLogin = () => {
-    api
-      .login({
-        username: "admin",
-        password: "admin",
-      })
-      .then((response: any) => {
-        saveToken(response.data);
-        navigate("tasks");
-      });
+    api.login(login).then((response: any) => {
+      saveToken(response.data);
+      navigate("tasks");
+    });
   };
 
   return (
     <>
-      <h1>Login Page</h1>
-      <input type="text" />
-      <input type="text" />
-      <button onClick={handleLogin}>Login</button>
+      <h1 className={styles.h1}>
+        Bem vindo <br /> de volta!
+      </h1>
+      <div className={styles.form}>
+        <div className={styles.form_section}>
+          <label>Username</label>
+          <input
+            type="text"
+            onChange={(e) => setLogin({ ...login, password: e.target.value })}
+            required
+          />
+        </div>
+        <div className={styles.form_section}>
+          <label>Password</label>
+          <input
+            type="text"
+            onChange={(e) => setLogin({ ...login, username: e.target.value })}
+            required
+          />
+        </div>
+
+        <div className={styles.form_section}>
+          <button onClick={handleLogin}>Login</button>
+          <p className={styles.register}>
+            Não possuí conta?{" "}
+            <a href="/register">
+              <strong>Registre-se</strong>
+            </a>
+          </p>
+        </div>
+      </div>
     </>
   );
 };
