afcc0f8aa5842f819b7ea1940b18ba98252bf7b7
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-04-12 08:11:04 -0300

    fix: url api

--- webapp/src/api/api.ts
+++ webapp/src/api/api.ts
@@ -4,7 +4,7 @@ import LoginResponse from "../pages/login/types";
 import RegisterResponse from "../pages/register/types";
 
 export default class Api {
-  private url = "http://172.25.0.3:3000";
+  private url = "http://localhost:7070";
 
   authorization = `Bearer ${isAuthenticated() !== null ? getToken() : ""}`;
 
